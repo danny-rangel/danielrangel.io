1b560ff3fcb06cd1e7dd72f62d6ca249a9de2754
Danny Rangel <danielrangel.tech@gmail.com>
2019-03-07 02:44:36 -0800

    modify header

--- src/components/header.js
+++ src/components/header.js
@@ -37,7 +37,7 @@ const Header = ({ siteTitle }) => (
               {siteTitle}
             </Link>
           </h2>
-          <h5 style={{ color: '#FFCCBC', margin: 0}}>I create through code and music</h5>
+          <h5 style={{ color: '#FFCCBC', margin: 0}}>Creating with code and music</h5>
         </div>
       </div>
     </HeaderContainer>
