9cb19f50078dad7822b3c7006b800cd13c60b96d
Danny Rangel <danielrangel.tech@gmail.com>
2019-03-20 11:50:23 -0700

    fixed footer

--- src/components/blogPage.js
+++ src/components/blogPage.js
@@ -11,7 +11,7 @@ const BlogPage = () => (
         <Layout>
             <BlogListing />
         </Layout>
-        <footer style={{color: '#FFCCBC', bottom: 0, width: '100%', textAlign: 'center', left: 0, position: 'absolute'}}>
+        <footer style={{color: '#FFCCBC', bottom: 0, width: '100%', textAlign: 'center', left: 0, position: 'relative'}}>
             <div>
                 <h4 style={{display: 'inline-block', margin: '8px'}} ><a href="https://github.com/danny-rangel" style={{color: '#FFCCBC'}} >Github</a></h4>
                 <h4 style={{display: 'inline-block', margin: '8px'}}><a href="mailto:hello@danielrangel.io" style={{color: '#FFCCBC'}} >Email</a></h4>
