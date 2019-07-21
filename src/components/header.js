bbec192a87ff2eb2db285272c9a88a2676cfa4a8
Danny Rangel <danielrangel.tech@gmail.com>
2019-07-20 18:45:56 -0700

    removed avi

--- src/components/header.js
+++ src/components/header.js
@@ -1,19 +1,19 @@
 import { Link } from "gatsby"
 import PropTypes from "prop-types"
 import React from "react"
-import styled from 'styled-components';
+import styled from "styled-components"
 // import Img from "gatsby-image"
-import avi from '../images/profilepic.jpg';
+// import avi from "../images/profilepic.jpg"
 
 const HeaderWrapper = styled.div`
   margin-bottom: 1.45rem;
-`;
+`
 
 const HeaderContainer = styled.div`
   margin: 0 auto;
   max-width: 960;
-  padding: 0.8rem;
-`;
+  padding: 16px;
+`
 
 // const AVI_QUERY = graphql`
 // {
@@ -32,30 +32,33 @@ const HeaderContainer = styled.div`
 const Header = ({ siteTitle }) => (
   <HeaderWrapper>
     <HeaderContainer>
-      <div style={{
-            margin: `0 auto`,
-            maxWidth: 960,
-            padding: `0px 0.8rem`,
-            paddingTop: 0,
-          }}>
+      <div
+        style={{
+          margin: `0 auto`,
+          maxWidth: 960,
+          padding: 0,
+        }}
+      >
         {/* <StaticQuery
           query={AVI_QUERY}
           render={data => <Img className="aviPhoto" fluid={data.file.childImageSharp.fluid} />}
         /> */}
-        <img className="aviPhoto" alt="avatar" src={avi} ></img>
-        <div style={{display: 'inline-block', padding: '0 0 0 1rem'}}>
+        {/* <img className="aviPhoto" alt="avatar" src={avi} ></img> */}
+        <div style={{ display: "inline-block", padding: "0 0 0 1rem" }}>
           <h2 style={{ margin: 0 }}>
             <Link
               to="/"
               style={{
                 textDecoration: `none`,
-                color: '#FFCCBC'
+                color: "#FFCCBC",
               }}
             >
               {siteTitle}
             </Link>
           </h2>
-          <h5 style={{ color: '#FFCCBC', margin: 0}}>Creating through code and music</h5>
+          <h5 style={{ color: "#FFCCBC", margin: 0 }}>
+            Creating through code and music
+          </h5>
         </div>
       </div>
     </HeaderContainer>
