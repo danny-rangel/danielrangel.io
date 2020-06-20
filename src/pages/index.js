cd5e82315335acc851901b1ab1aa20ee8ce793de
Danny Rangel <danielrangel.tech@gmail.com>
2020-06-20 15:59:01 -0700

    removed music link

--- src/pages/index.js
+++ src/pages/index.js
@@ -13,12 +13,6 @@ const IndexPage = () => (
       </Link>
     </h2>
     <p> A collection of some personal projects</p>
-    <h2 style={{ margin: "0 0 10px" }}>
-      <Link to="/music" style={{ textDecoration: "none", color: "#FFCCBC" }}>
-        Music
-      </Link>
-    </h2>
-    <p>Recordings by yours truly</p>
   </Layout>
 )
 
